a785c0bb311beae201ccc7562a7d59d3cdecbf60
riteshkshik <stevenfrayne12344@gmail.com>
2023-07-18 15:32:10 +0530

    first commit

---
+++ api/server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+
+app.use(express.json());   /// helps to handle res.json() data coming inside the body
+app.use(cors());
+
+mongoose.connect("mongodb://127.0.0.1:27017/mern-todo",{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => console.log("Connected To DB")).catch(console.error);
+
+const Todo = require('./models/Todo');
+
+app.get('/todos', async (req, res) =>{
+    const todos = await Todo.find();
+    res.json(todos);
+});
+
+app.post('/todo/new', (req, res) =>{
+    const newTodo = new Todo({
+        text: req.body.text
+    });
+    // return;
+    newTodo.save();
+    res.json(newTodo);
+});
+
+app.delete('/todo/delete/:id', async (req, res) =>{
+    const deleted_todo = await Todo.findByIdAndDelete(req.params.id);
+    res.json(deleted_todo);
+});
+
+app.put('/todo/complete/:id', async (req, res) =>{
+    const todo = await Todo.findById(req.params.id);
+    todo.complete = !todo.complete;
+    todo.save();
+    res.json(todo);
+})
+
+app.listen(3001, () => console.log("Server Started on Port 3001"));
---
+++ client/src/App.js
@@ -0,0 +1,105 @@
+import { useState, useEffect } from "react";
+
+const API_BASE = "http://localhost:3001";
+
+function App() {
+
+  const [todos, setTodos] = useState([]);
+  const [popupActive, setPopupActive] = useState(false);
+  const [newTodo, setNewTodo] = useState("");
+
+  useEffect(() =>{
+    GetTodos();
+
+    // console.log(todos);
+  }, []);
+
+  const GetTodos = () => {
+    fetch("http://localhost:3001/todos")
+      .then((res) =>{
+        // console.log(res);
+        return res.json();
+      }).then((data) => setTodos(data)).catch(err => console.error("error: ", err));
+  }
+
+  function completeTodo(todoId){
+    fetch(`http://localhost:3001/todo/complete/${todoId}`,{method: 'PUT'})
+      .then(res =>{
+        return res.json();
+      })
+      .then(data => {
+        setTodos(
+          todos.map(todo =>{
+            if(todo._id === data._id){
+              todo.complete = data.complete;
+            }
+            return todo;
+          })
+        )
+      })
+  }
+
+  const AddTodo = async () => {
+    const data = await fetch("http://localhost:3001/todo/new", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        text: newTodo
+      })
+    }).then(res => res.json())
+
+    setTodos([...todos, data]);
+    setPopupActive(false);
+    setNewTodo("");
+  }
+
+  function deleteTodo(todoId, event){
+    event.stopPropagation();
+    fetch(`http://localhost:3001/todo/delete/${todoId}`,{method: 'DELETE'})
+      .then(res =>{
+        return res.json();
+      })
+      .then(data => {
+        setTodos(todos => todos.filter(todo => todo._id !== data._id));
+      })
+  }
+
+  return (
+    <div className="App">
+      <h1>Welcome Ritesh</h1>
+      <h4>Your Tasks</h4>
+      <div className="todos">
+
+        {
+          todos.map(todo => (
+              <div className={
+                "todo " + (todo.complete ? "is-complete": "") 
+                } key={todo._id} onClick={() => completeTodo(todo._id)}>
+                <div className="checkbox"></div>
+                <div className="text">{todo.text}</div>
+                <div className="delete-todo" onClick={(event) => deleteTodo(todo._id, event)}>x</div>
+            </div>
+          ))
+        }
+      </div>
+
+      <div className="addPopup" onClick={() => setPopupActive(true)}>+</div>
+      { popupActive ? (
+        <div className="popup">
+          <div className="closePopup" onClick={() => setPopupActive(false)}>x</div>
+          <div className="content">
+            <h3>Add Task</h3>
+            <input autoFocus='true' type="text" className="add-todo-input" 
+                   onChange={e => setNewTodo(e.target.value)} value={newTodo} onKeyPress={(e) => e.key === 'Enter' ? AddTodo() : null}/>
+            <button className="button" onClick={AddTodo}>Create Todo</button>
+          </div>
+        </div>
+      ) : '' }
+
+    </div>
+  );
+}
+
+export default App;
